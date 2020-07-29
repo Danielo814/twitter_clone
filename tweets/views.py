c968b254a237ab981a33423b119dc3b2d4b5addf
Daniel Ojeda <ojedadaniel92@gmail.com>
2020-07-28 17:00:03 -0700

    javascript logic to access data from other endpoints from home route

--- tweets/views.py
+++ tweets/views.py
@@ -7,6 +7,16 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello, World!</h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content} for x in qs]
+    data = {
+        'response': tweets_list,
+        'isUser': False
+    }
+    return JsonResponse(data)
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
@@ -15,7 +25,7 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     data = {
         "id": tweet_id,
-    }
+  }
     status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
